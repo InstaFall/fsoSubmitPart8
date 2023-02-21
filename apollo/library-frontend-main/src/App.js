02432b00be697ca9d226209a0da3609ebd93bd02
InstaFall <gorkemcanakilli@hotmail.com>
2023-02-21 14:28:01 +0300

    Commit exercise 8.2

--- apollo/library-frontend-main/src/App.js
+++ apollo/library-frontend-main/src/App.js
@@ -5,18 +5,19 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from './queries'
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const authorsQuery = useQuery(ALL_AUTHORS)
-  if (authorsQuery.loading) return <div>Loading..</div>
+  const booksQuery = useQuery(ALL_BOOKS)
+
+  if (authorsQuery.loading || booksQuery.loading) return <div>Loading..</div>
 
   return (
     <>
       <div>
         <Link to="/authors">
-          {' '}
           <button onClick={() => setPage('authors')}>authors</button>
         </Link>
         <Link to="/books">
@@ -37,7 +38,12 @@ const App = () => {
             />
           }
         />
-        <Route path="/books" element={<Books show={page === 'books'} />} />
+        <Route
+          path="/books"
+          element={
+            <Books books={booksQuery.data.allBooks} show={page === 'books'} />
+          }
+        />
         <Route path="/newbook" element={<NewBook show={page === 'add'} />} />
       </Routes>
     </>
